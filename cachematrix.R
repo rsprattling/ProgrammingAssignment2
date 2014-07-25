101672f55ca785168d2c7dcf4c597152e28a2e21
R. Sprattling <rsprattling@gmail.com>
2014-07-25 15:59:02 -0400

    Improve comments and formatting

--- cachematrix.R
+++ cachematrix.R
@@ -5,45 +5,49 @@
 ##              exist.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
-  ##  Set the matrix data. Note that if matrix is changed (via set fn) then any cached
-  ##  inverse is NULLed
-  set <- function(y) {
-    x <<- y
-    inverse <<- NULL
-  }
-  ##  Return the matrix.
-  get <- function() x
-  
-  ## Cache the inverse
-  setinverse <- function(i) {
-    inverse <<- i  ##Set the inverse.
-  }
-  
-  ## Get the currently cached inverse    
-  getinverse <- function() inverse
-  
-  ## Return the method instances and their implicit data objects.
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+    
+    inverse <- NULL
+    
+    ##  Set the matrix data. Note that if matrix is changed (via set fn) then any cached
+    ##  inverse is NULLed
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    ##  Get the matrix.
+    get <- function() x
+    
+    ## Cache the inverse
+    setinverse <- function(i) {
+        inverse <<- i  ##Set the inverse.
+    }
+    
+    ## Get the currently cached inverse    
+    getinverse <- function() inverse
+    
+    ## Return the method instances and their implicit data objects.
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
-
 
 
 ## cacheSolve - Return the inverse of a matrix, caching the inverse if it doesn't already
 ##              exist.
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
-  if(!is.null(i)) {
-    message("getting cached inverse")
-    return(i)
-  }
-  ## Meh. No inverse yet exists, so calculate it, set it and return it.
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
+    
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached inverse")
+        return(i)
+    }
+    
+    ## Meh. No inverse yet exists, so calculate it, set it and return it.
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
